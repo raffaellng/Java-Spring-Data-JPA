eb2e0a848c8622437d8eac014396e90f0a978aa7
Raffaell Negreiros <raffaell.negreiros@levva.io>
2023-04-06 18:10:52 -0300

    Adicionando as Entidades

---
+++ src/main/java/com/api/rasfood/entity/Cliente.java
@@ -0,0 +1,85 @@
+package com.api.rasfood.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Embedded
+    private ClienteId clienteId;
+    private String nome;
+
+    @Embedded
+    private Contato contato;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Endereco> enderecoList = new ArrayList<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(String cpf, String email, String nome) {
+        this.clienteId = new ClienteId(email,cpf);
+        this.nome = nome;
+    }
+
+    public void addEndereco(Endereco endereco) {
+        endereco.setCliente(this);
+        this.enderecoList.add(endereco);
+    }
+
+    public String getEmail() {
+        return clienteId.getEmail();
+    }
+
+    public void setEmail(String email) {
+        this.clienteId.setEmail(email);
+    }
+
+    public String getCpf() {
+        return clienteId.getCpf();
+    }
+
+    public void setCpf(String cpf) {
+        this.clienteId.setCpf(cpf);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Endereco> getEnderecoList() {
+        return enderecoList;
+    }
+
+    public void setEnderecoList(List<Endereco> enderecoList) {
+        this.enderecoList = enderecoList;
+    }
+
+    public Contato getContato() {
+        return contato;
+    }
+
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "cpf='" + clienteId.getCpf() + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + clienteId.getEmail() + '\'' +
+                ", contato=" + contato +
+                ", enderecoList=" + enderecoList +
+                '}';
+    }
+}
---
+++ src/main/java/com/api/rasfood/entity/Ordem.java
@@ -0,0 +1,92 @@
+package com.api.rasfood.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ordens")
+public class Ordem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal = new BigDecimal(0);
+
+    @Column(name = "data_de_criacao")
+    private LocalDateTime dataDeCriacao = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "ordem", cascade = CascadeType.ALL)
+    private List<OrdensCardapio> ordensCardapioList = new ArrayList<>();
+
+    public Ordem() {
+    }
+
+    public Ordem(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void addOrdensCardapio(OrdensCardapio ordensCardapio) {
+        ordensCardapio.setOrdem(this);
+        this.ordensCardapioList.add(ordensCardapio);
+        this.valorTotal = valorTotal.add(ordensCardapio.getValorDeRegistro()
+                .multiply(BigDecimal.valueOf(ordensCardapio.getQuantidade())));
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<OrdensCardapio> getOrdensCardapioList() {
+        return ordensCardapioList;
+    }
+
+    public void setOrdensCardapioList(List<OrdensCardapio> ordensCardapioList) {
+        this.ordensCardapioList = ordensCardapioList;
+    }
+
+    @Override
+    public String toString() {
+        return "Ordem{" +
+                "id=" + id +
+                ", valorTotal=" + valorTotal +
+                ", dataDeCriacao=" + dataDeCriacao +
+                ", cliente=" + cliente +
+                ", ordensCardapioList=" + ordensCardapioList +
+                '}';
+    }
+}
